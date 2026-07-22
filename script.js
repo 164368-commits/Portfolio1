bfd44e657a9119af125ab86105175c76a2807429
Rp1331 <164368@wsk.ac.th>
2026-07-22 23:52:35 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,24 @@
 const music = document.getElementById("music");
 const btn = document.getElementById("btn");
+const img = document.querySelector(".image-container img");
 
 function toggleMusic() {
     if (music.paused) {
         music.play();
         btn.innerHTML = "⏸ หยุดฟัง";
+        btn.style.background = "#9C27B0"; // สีม่วง
+        img.style.animation = "spin 10s linear infinite";
     } else {
         music.pause();
         btn.innerHTML = "▶ คลิกเพื่อฟัง";
+        btn.style.background = "#ff4081"; // สีชมพู
+        img.style.animation = "";
     }
 }
 
-// เมื่อเพลงเล่นจบ ให้ปุ่มกลับเป็นเหมือนเดิม
+// เมื่อเพลงจบ
 music.addEventListener("ended", function () {
     btn.innerHTML = "▶ คลิกเพื่อฟัง";
+    btn.style.background = "#ff4081";
+    img.style.animation = "";
 });
